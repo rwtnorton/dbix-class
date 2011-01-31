160b62e2cb8a14e62825d60b77ab66dd8aee0076
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2011-01-31 13:39:46 -0600

    move SQLite datemath code to the correct SQLMaker class

--- lib/DBIx/Class/SQLMaker/SQLite.pm
+++ lib/DBIx/Class/SQLMaker/SQLite.pm
@@ -9,4 +9,24 @@ use Carp::Clan qw/^DBIx::Class|^SQL::Abstract/;
 # Disable it here
 sub _lock_select () { '' };
 
+
+{
+  my %part_map = (
+     month        => 'm',
+     day_of_month => 'd',
+     year         => 'Y',
+  );
+
+  sub _datetime_sql { "STRFTIME('$part_map{$_[1]}', $_[2])" }
+}
+
+sub _datetime_diff_sql {
+   my ($self, $part, $left, $right) = @_;
+   '(' .
+      $self->_datetime_sql($part, $left)
+       . ' - ' .
+      $self->_datetime_sql($part, $right)
+   . ')'
+}
+
 1;
